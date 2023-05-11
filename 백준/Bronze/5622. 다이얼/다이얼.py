25d196128a905faccea0914d4dae75f558c6b212
kzoone <133111757+kzoone@users.noreply.github.com>
2023-05-11 15:58:02 +0900

    [Bronze II] Title: 다이얼, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,9 @@
+alphabet = input()
+dial = ["ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"]
+time = 0
+for i in range(len(alphabet)):
+    for j in dial:
+        if alphabet[i] in j:
+            time += dial.index(j)+3
+print(time)
+            
