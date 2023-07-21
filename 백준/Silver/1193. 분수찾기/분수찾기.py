c6701e31d3c7c11e530c59a60c4f8dab9416c63a
kzoone <133111757+kzoone@users.noreply.github.com>
2023-07-21 17:46:47 +0900

    [Silver V] Title: 분수찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,17 +1,15 @@
 n = int(input())
+line = 1
 
-line = 0
-end = 0
-while n > end:
+while n > line:
+    n -= line
     line += 1
-    end += line
 
-diff = end - n
-if line%2 == 0: #짝수 라인 일때
-    top = line - diff
-    bottom = diff + 1
+if line % 2 == 0:
+    up = n
+    down = line - n + 1
 else:
-    top = diff + 1
-    bottom = line - diff
+    up = line - n + 1
+    down = n
 
-print("%d/%d"%(top,bottom))
+print(up,'/',down, sep="")
