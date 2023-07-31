c7e2d9b0cf37761b0f5ab6c519c8ba820722faf4
kzoone <133111757+kzoone@users.noreply.github.com>
2023-07-31 18:06:54 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 84 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,26 +1,29 @@
-N, M = map(int, input().split())
-original = []
-count = []
-
-for _ in range(N):
-    original.append(input())
-
-for a in range(N-7):
-    for b in range(M-7):
-        index1 = 0
-        index2 = 0
-        for i in range(a, a+8):
-            for j in range(b, b+8):
-                if (i+j) % 2 == 0:
-                    if original[i][j] != 'W':
-                        index1 += 1
-                    if original[i][j] != 'B':
-                        index2 += 1
+n, m = map(int, input().split())
+board = []
+result = []
+ 
+for _ in range(n):
+    board.append(input())
+ 
+for i in range(n-7):
+    for j in range(m-7):
+        draw1 = 0
+        draw2 = 0
+ 
+        for a in range(i, i+8):
+            for b in range(j, j+8):
+                if (a + b) % 2 == 0:
+                    if board[a][b] != 'B':
+                        draw1 += 1
+                    if board[a][b] != 'W':
+                        draw2 += 1
                 else:
-                    if original[i][j] != 'B':
-                        index1 += 1
-                    if original[i][j] != 'W':
-                        index2 += 1
-        count.append(min(index1, index2))
-
-print(min(count))
+                    if board[a][b] != 'W':
+                        draw1 += 1
+                    if board[a][b] != 'B':
+                        draw2 += 1
+ 
+        result.append(draw1)
+        result.append(draw2)
+ 
+print(min(result))
