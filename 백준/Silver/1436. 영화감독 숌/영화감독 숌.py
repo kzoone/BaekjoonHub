b63971cf4f591ff5879cdd1ee83425a2e180ddf2
kzoone <133111757+kzoone@users.noreply.github.com>
2023-08-01 10:16:39 +0900

    [Silver V] Title: 영화감독 숌, Time: 564 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,10 +1,12 @@
 n = int(input())
-cnt = 0
-six_n = 666
-while True:
-    if '666' in str(six_n):
-        cnt += 1
-    if cnt == n:
-        print(six_n)
-        break
-    six_n += 1
+
+theEndNumber = 666 #종말번호
+count = 0 # 종말번호를 업데이트 시켜줄 카운트값
+while True: #무한반복
+    if '666' in str(theEndNumber): #만약 종말번호안에 666이 포함되어 있으면
+        count += 1 #해당 카운트값을 1 증가
+        if count == n: #그 카운트값이 입력값과 같다면
+            break #반복문 탈출
+    theEndNumber += 1 #종말번호를 계속해서 1씩 증가
+
+print(theEndNumber) #입력한 n번째의 종말번호 출력
