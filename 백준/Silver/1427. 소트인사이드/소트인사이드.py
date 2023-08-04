7323939823e3533d9cd4f4fc38204e6a7674184b
kzoone <133111757+kzoone@users.noreply.github.com>
2023-08-04 21:04:48 +0900

    [Silver V] Title: 소트인사이드, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.py
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.py
@@ -1,11 +1,4 @@
-n = int(input())
- 
-li = []
-for i in str(n):
-    li.append(int(i))
-# li = list(map(int,str(n))) 으로 변경가능
-    
-li.sort(reverse=True) # 내림차순
- 
-for i in li:
+nums = list(map(int, str(input())))
+nums.sort(reverse=True)
+for i in nums:
     print(i,end='')
